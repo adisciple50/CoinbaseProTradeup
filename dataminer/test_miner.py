c3b21ce53d582a3486cf185e93a2bd83d08fb572
Ben Cullen <Holographic.sol@gmail.com>
2022-06-08 00:34:06 +0100

    test_miner.py is done for now

--- dataminer/test_miner.py
+++ dataminer/test_miner.py
@@ -1,11 +1,30 @@
 import unittest
 from dataminer.miner import Miner
+from dataminer.product import Quote
+from cbpro import PublicClient
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
+    def test_load_quote(self):
+        self.assertIsInstance(Miner().load_quote('BTC-GBP'),Quote)
 
+    def test_load_quotes(self):
+        quotes = Miner().load_quotes()
+        self.assertIsInstance(quotes, list)
+        self.assertEqual(len(quotes), len(PublicClient.get_products()))
+
+    def test_construct_tradeup_sequence(self):
+        sequence = Miner().construct_tradeup_sequence()
+        self.assertEqual(len(sequence), 4)
+        for item in sequence:
+            self.assertIsInstance(item,str)
+
+    def get_tradeup_sequences(self):
+        self.assertIsInstance(Miner().get_tradeup_sequence(),list)
+
+    def test_parse_for_database(self):
+        self.assertIsInstance(Miner().parse_for_dateabase(), list)
+        self.assertIsInstance(Miner().parse_for_dateabase()[0], tuple)
 
 
 if __name__ == '__main__':
