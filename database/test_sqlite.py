8ff34e92230c96a87cb8b736fa0a9953b8e0194a
Ben Cullen <Holographic.sol@gmail.com>
2022-06-08 04:32:49 +0100

    added create and delete tests

--- database/test_sqlite.py
+++ database/test_sqlite.py
@@ -4,6 +4,18 @@ from dataminer.miner import Miner
 from database.sqlite import Storage
 
 class MyTestCase(unittest.TestCase):
+    def test_create_table_if_not_exists(self):
+        db = sqlite3.connect('test.db')
+        Storage.create_table_if_not_exists()
+        db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='quotes'")
+        self.assertGreater(db.cursor().fetchone()[0],0)
+
+    def test_delete_table(self):
+        db = sqlite3.connect('test.db')
+        Storage.delete_table()
+        db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='quotes'")
+        self.assertEqual(db.cursor().fetchone()[0], 0)
+
     def test_data_has_been_inserted(self):
         db = sqlite3.connect('test.db')
         Storage('test.db').bulk_insert(Miner().parse_quotes_for_database())
