a97f9676c1163d2ce88c5a9f59a56233b8ae94d6
Ben Cullen <Holographic.sol@gmail.com>
2022-06-08 01:10:40 +0100

    test_sqlite.py.py is done for now

---
+++ database/test_sqlite.py
@@ -0,0 +1,21 @@
+import unittest
+import sqlite3
+from dataminer.dataminer import Miner
+from database.sqlite import Storage
+
+class MyTestCase(unittest.TestCase):
+    def test_data_has_been_inserted(self):
+        db = sqlite3.connect('test.db')
+        Storage('test.db').bulk_insert(Miner().parse_quotes_for_database())
+        quotes = db.execute('SELECT * FROM quotes')
+        self.assertGreater(len(quotes),0)
+
+    def test_data_has_been_truncated(self):
+        Storage('test.db').bulk_insert(Miner().parse_quotes_for_database())
+        Storage('test.db').truncate()
+        db = sqlite3.connect('test.db')
+        quotes = db.execute('SELECT * FROM quotes')
+        self.assertGreater(len(quotes), 0)
+
+if __name__ == '__main__':
+    unittest.main()
